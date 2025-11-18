279f7991c966bb999d86d485f8834f72c5b0fda9
Francis-Rav <heyrosa.raven@gmail.com>
2025-11-18 20:20:11 +0800

    MathOperators

---
+++ MathOperators.py
@@ -0,0 +1,10 @@
+x= int(input("Enter number: "))
+y= int(input("Enter number: "))
+print("Your numbers are", x , y )
+sum = (x + y)
+print("the sum is:", x + y)
+
+
+a = True
+b = False
+print (10 == 5 )
