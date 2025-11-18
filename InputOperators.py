607d903316d632ef48b546cc5dd21767c63ce8dc
Francis-Rav <heyrosa.raven@gmail.com>
2025-11-18 20:17:07 +0800

    InputOperators

---
+++ InputOperators.py
@@ -0,0 +1,12 @@
+name= str(input("Enter name: "))
+address= str(input("Enter address: "))
+age= int(input("Enter age: "))
+course= str(input("Enter course: "))
+age = str(age)
+
+print(f"my name is {name}, i am this old {age}, i live in {address}, i study {course}")
+
+print("name: ", name.upper())
+print(len(address))
+print(age + " is a string trust")
+print("course: ", course.lower())
